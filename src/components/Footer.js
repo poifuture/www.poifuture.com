c6564d3fa8ff60b6bf83da3f58d4030184b7948a
Nagi <nagi@poifuture.com>
2019-07-09 23:28:03 +0800

    Apply coddle styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,113 +1,204 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import facebook from "../img/social/facebook.svg"
+import instagram from "../img/social/instagram.svg"
+import twitter from "../img/social/twitter.svg"
+import vimeo from "../img/social/vimeo.svg"
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
+      <div>
+        <section className="footer-bar">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-3 col-md-6">
+                <figure>
+                  <img src="images/contact-icon01.png" alt="" />
+                </figure>
+                <h4>E-MAILS</h4>
+                <address>info@coddle.com.br</address>
               </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
+              {/* <!-- end col-3 --> */}
+              <div className="col-lg-3 col-md-6">
+                <figure>
+                  <img src="images/contact-icon02.png" alt="" />
+                </figure>
+                <h4>PHONES</h4>
+                <address>+380 (98) 294 80 86</address>
               </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
+              {/* <!-- end col-3 --> */}
+              <div className="col-lg-3 col-md-6">
+                <figure>
+                  <img src="images/contact-icon03.png" alt="" />
+                </figure>
+                <h4>SUPPORT</h4>
+                <address>Mon to Fri 09:00h to 18:00h</address>
+              </div>
+              {/* <!-- end col-3 --> */}
+              <div className="col-lg-3 col-md-6">
+                <figure>
+                  <img src="images/contact-icon04.png" alt="" />
+                </figure>
+                <h4 style={{ display: "none" }}>LOCATION</h4>
+                <address>
+                  2 ZGC Software Park Ste. 2240-049 <br /> Haidian, Beijing,
+                  China
+                </address>
+              </div>
+              {/* <!-- end col-3 -->  */}
+            </div>
+            {/* <!-- end row -->  */}
+          </div>
+          {/* <!-- end container -->  */}
+        </section>
+        {/* <!-- end footer-bar --> */}
+        <footer className="footer">
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                {/* <img src="images/logo-light.png" alt="Image" className="logo" /> */}
+                <h1>poifuture</h1>
+                <p>
+                  Poifuture products are continually improving everyone's
+                  fantastic life.
+                </p>
+                <ul>
+                  <li>
+                    <a href="/">Bilibili</a>
+                  </li>
+                  <li>
+                    <a href="/">YouTube</a>
+                  </li>
+                  <li>
+                    <a href="/">Instagram</a>
+                  </li>
+                  <li>
+                    <a href="/">Facebook</a>
+                  </li>
+                  <li>
+                    <a href="/">Twitter</a>
+                  </li>
+                  <li>
+                    <a href="/">WeChat</a>
+                  </li>
+                  <li>
+                    <a href="/">Weibo</a>
+                  </li>
+                </ul>
+                <small>
+                  Copyright © 2018 - 2019 Poifuture Inc. | All Rights Reserved.
+                  | <span>京 ICP 备 XXX</span>
+                </small>
+              </div>
+              {/* <!-- end col-12 -->  */}
+            </div>
+            {/* <!-- end row -->  */}
+          </div>
+          {/* <!-- end container -->  */}
+        </footer>
+        {/* <!-- end footer -->  */}
+
+        <div
+          className="footer has-background-black has-text-white-ter"
+          style={{ display: "none" }}
+        >
+          <div className="content has-text-centered">
+            <h1 style={{ color: "white" }}>poifuture</h1>
+          </div>
+          <div className="content has-text-centered has-background-black has-text-white-ter">
+            <div className="container has-background-black has-text-white-ter">
+              <div className="columns">
+                <div className="column is-4">
+                  <section className="menu">
+                    <ul className="menu-list">
+                      <li>
+                        <Link to="/" className="navbar-item">
+                          Home
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="navbar-item" to="/about">
+                          About
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="navbar-item" to="/products">
+                          Products
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="navbar-item" to="/contact/examples">
+                          Form Examples
+                        </Link>
+                      </li>
+                      <li>
+                        <a
+                          className="navbar-item"
+                          href="/admin/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Admin
+                        </a>
+                      </li>
+                    </ul>
+                  </section>
+                </div>
+                <div className="column is-4">
+                  <section>
+                    <ul className="menu-list">
+                      <li>
+                        <Link className="navbar-item" to="/blog">
+                          Latest Stories
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="navbar-item" to="/contact">
+                          Contact
+                        </Link>
+                      </li>
+                    </ul>
+                  </section>
+                </div>
+                <div className="column is-4 social">
+                  <a title="facebook" href="https://facebook.com">
+                    <img
+                      className="fas fa-lg"
+                      src={facebook}
+                      alt="Facebook"
+                      style={{ width: "1em", height: "1em" }}
+                    />
+                  </a>
+                  <a title="twitter" href="https://twitter.com">
+                    <img
+                      className="fas fa-lg"
+                      src={twitter}
+                      alt="Twitter"
+                      style={{ width: "1em", height: "1em" }}
+                    />
+                  </a>
+                  <a title="instagram" href="https://instagram.com">
+                    <img
+                      src={instagram}
+                      alt="Instagram"
+                      style={{ width: "1em", height: "1em" }}
+                    />
+                  </a>
+                  <a title="vimeo" href="https://vimeo.com">
+                    <img
+                      src={vimeo}
+                      alt="Vimeo"
+                      style={{ width: "1em", height: "1em" }}
+                    />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </footer>
+      </div>
     )
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react"
+import { Link } from "gatsby"
+import github from "../img/github-icon.svg"
+import logo from "../img/logo.svg"
 
 const Navbar = class extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       active: false,
-      navBarActiveClass: '',
+      navBarActiveClass: "",
     }
   }
 
@@ -23,10 +23,10 @@ const Navbar = class extends React.Component {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
+              navBarActiveClass: "is-active",
             })
           : this.setState({
-              navBarActiveClass: '',
+              navBarActiveClass: "",
             })
       }
     )
@@ -34,26 +34,304 @@ const Navbar = class extends React.Component {
 
   render() {
     return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
+      <div>
+        {/* begin mobile menu */}
+        <aside className="hamburger-menu-box">
+          <div className="menu">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a href="/" className="nav-link transition">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">RETAIL</a>
+                <ul>
+                  <li>
+                    <a>
+                      <span style={{ textDecoration: "line-through" }}>
+                        AINCRAD
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a>
+                      <span style={{ textDecoration: "line-through" }}>
+                        LAMBENT
+                      </span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">EDUCATION</a>
+                <ul>
+                  <li>
+                    <a
+                      href="https://github.com/poifuture/poigoi"
+                      className="transition"
+                    >
+                      POI GOI
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Productivity</a>
+                <ul>
+                  <li>
+                    <a
+                      href="https://github.com/poifuture/markdown-styler-for-word"
+                      className="transition"
+                    >
+                      Markdown Styler
+                    </a>
+                  </li>
+                  <li style={{ display: "none" }}>
+                    <a href="works-nonspaced-grid.html" className="transition">
+                      NONSPACED GRID
+                    </a>
+                  </li>
+                  <li style={{ display: "none" }}>
+                    <a href="works-masonry-grid.html" className="transition">
+                      MASONRY GRID
+                    </a>
+                  </li>
+                  <li style={{ display: "none" }}>
+                    <a href="works-retro-grid.html" className="transition">
+                      RETRO GRID
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link to="/blog" className="nav-link transition">
+                  NEWS
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link transition">
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+            {/* <!-- end navbar-nav -->  */}
+          </div>
+          {/* <!--end menu --> */}
+          <div className="inner">
+            <div className="swiper-slider">
+              <div className="swiper-wrapper">
+                <div className="swiper-slide">
+                  <img src="images/office01.jpg" alt="" />
+                </div>
+                {/* <!-- end swiper-slide --> */}
+                <div className="swiper-slide">
+                  <img src="images/office02.jpg" alt="" />
+                </div>
+                {/* <!-- end swiper-slide --> */}
+                <div className="swiper-slide">
+                  <img src="images/office03.jpg" alt="" />
+                </div>
+                {/* <!-- end swiper-slide -->  */}
+              </div>
+              {/* <!-- end swiper-wrapper --> */}
+              <div className="swiper-pagination"></div>
+              {/* <!--end swiper-pagination -->  */}
+            </div>
+            {/* <!--end swiper-slider --> */}
+            <p className="lead">Are you a Chūnibyō patient?</p>
+            <a href="/" className="ghost-btn">
+              <svg>
+                <defs>
+                  <linearGradient>
+                    <stop offset="0%" stopColor="#FF8282" />
+                    <stop offset="100%" stopColor="#E178ED" />
+                  </linearGradient>
+                </defs>
+                <rect
+                  width="206"
+                  height="50"
+                  x="5"
+                  y="5"
+                  rx="25"
+                  fill="none"
+                  stroke="#ffffff"
+                ></rect>
+              </svg>
+              <span>Login</span>
+            </a>
+          </div>
+          {/* <!-- end inner -->  */}
+        </aside>
+        {/* <!-- end hamburger-menu-box --> */}
+        {/* end mobile menu */}
+        <div className="search-box">
+          <div className="inner">
+            <h4>SEARCH</h4>
+            <form>
+              <input type="text" placeholder="Type here to make search" />
+              <button type="submit">
+                <img src="images/search-icon.svg" alt="" />
+              </button>
+            </form>
+          </div>
+          {/* <!-- end inner -->  */}
+        </div>
+        {/* <!--end search-box --> */}
+
+        <header className="header">
+          <div id="particles-bg"></div>
+          {/* <!-- end particles-bg --> */}
+          <nav className="navbar">
+            <div className="logo">
+              <Link to="/" className="transition">
+                <img
+                  src="images/logo-round-100.png"
+                  alt=""
+                  className="logo-light"
+                />
+                <img
+                  src="images/logo-round-100.png"
+                  alt=""
+                  className="logo-colored"
+                />
+              </Link>
+            </div>
+            {/* <!-- end logo --> */}
+            <div className="hamburger-menu">
+              <span />
+              <span />
+              <span />
+            </div>
+            {/* <!-- hamburger-menu --> */}
+            <div className="search-btn" style={{ display: "none" }}></div>
+            {/* <!-- end search-btn -->  */}
+            <a href="/" className="custom-btn" style={{ display: "none" }}>
+              DRIBBBLE
+            </a>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a href="/" className="nav-link transition">
+                  HOME
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">RETAIL</a>
+                <ul>
+                  <li>
+                    <a>
+                      <span style={{ textDecoration: "line-through" }}>
+                        AINCRAD
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a>
+                      <span style={{ textDecoration: "line-through" }}>
+                        LAMBENT
+                      </span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">EDUCATION</a>
+                <ul>
+                  <li>
+                    <a
+                      href="https://github.com/poifuture/poigoi"
+                      className="transition"
+                    >
+                      POI GOI
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">PRODUCTIVITY</a>
+                <ul>
+                  <li>
+                    <a
+                      href="https://github.com/poifuture/markdown-styler-for-word"
+                      className="transition"
+                    >
+                      MARKDOWN STYLER
+                    </a>
+                  </li>
+                  <li style={{ display: "none" }}>
+                    <a href="works-nonspaced-grid.html" className="transition">
+                      NONSPACED GRID
+                    </a>
+                  </li>
+                  <li style={{ display: "none" }}>
+                    <a href="works-masonry-grid.html" className="transition">
+                      MASONRY GRID
+                    </a>
+                  </li>
+                  <li style={{ display: "none" }}>
+                    <a href="works-retro-grid.html" className="transition">
+                      RETRO GRID
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link to="/blog" className="nav-link transition">
+                  NEWS
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link transition">
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+            {/* <!-- end navbar-nav -->  */}
+          </nav>
+          {/* <!-- end navbar --> */}
+          {this.props.hero && this.props.hero.type === "screen" && (
+            <div className="inner">
+              <h2>{this.props.hero.title}</h2>
+              <p>{this.props.hero.subtitle}</p>
+              {this.props.hero.onClick && (
+                <a onClick={this.props.hero.onClick} className="ghost-btn">
+                  <svg>
+                    <defs>
+                      <linearGradient>
+                        <stop offset="0%" stopColor="#FF8282" />
+                        <stop offset="100%" stopColor="#E178ED" />
+                      </linearGradient>
+                    </defs>
+                    <rect
+                      width="206"
+                      height="50"
+                      x="5"
+                      y="5"
+                      rx="25"
+                      fill="none"
+                      stroke="#ffffff"
+                    ></rect>
+                  </svg>
+                  <span>Discover Now</span>
+                </a>
+              )}
+            </div>
+          )}
+          {/* <!-- end inner --> */}
+        </header>
+        {/* <!-- end header --> */}
+        <div className="container" style={{ display: "none" }}>
+          {/* <div className="container"> */}
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
               data-target="navMenu"
               onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
+            ></div>
           </div>
           <div
             id="navMenu"
@@ -90,7 +368,7 @@ const Navbar = class extends React.Component {
             </div>
           </div>
         </div>
-      </nav>
+      </div>
     )
   }
 }

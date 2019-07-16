272f1b7aba966d7028e1fd570355104905dbc59f
Nagi <nagi@poifuture.com>
2019-07-17 05:38:37 +0800

    Release

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,5 +1,11 @@
-window.nagiload = true
 ;(function($) {
+  var getScrollTop = function() {
+    return Math.max(
+      window.pageYOffset,
+      document.documentElement.scrollTop,
+      document.body.scrollTop
+    )
+  }
   $(document).ready(function() {
     "use strict"
 
@@ -8,122 +14,6 @@ window.nagiload = true
     if (window.matchMedia("(prefers-reduced-motion)").matches) {
       video.removeAttribute("autoplay")
       video.pause()
-    }
-
-    // PARTICLES BG EFFECT
-    var n = document.getElementById("particles-bg")
-    if (n == null) {
-    } else {
-      particlesJS("particles-bg", {
-        particles: {
-          number: {
-            value: 120,
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-          },
-          color: {
-            value: "#ffffff",
-          },
-          shape: {
-            type: "circle",
-            stroke: {
-              width: 0,
-              color: "#000000",
-            },
-            polygon: {
-              nb_sides: 5,
-            },
-            image: {
-              src: "img/github.svg",
-              width: 100,
-              height: 100,
-            },
-          },
-          opacity: {
-            value: 0.5,
-            random: false,
-            anim: {
-              enable: false,
-              speed: 1,
-              opacity_min: 0.1,
-              sync: false,
-            },
-          },
-          size: {
-            value: 3,
-            random: true,
-            anim: {
-              enable: false,
-              speed: 40,
-              size_min: 0.1,
-              sync: false,
-            },
-          },
-          line_linked: {
-            enable: false,
-            distance: 150,
-            color: "#ffffff",
-            opacity: 0.4,
-            width: 1,
-          },
-          move: {
-            enable: true,
-            speed: 6,
-            direction: "bottom",
-            random: false,
-            straight: false,
-            out_mode: "out",
-            bounce: false,
-            attract: {
-              enable: false,
-              rotateX: 600,
-              rotateY: 1200,
-            },
-          },
-        },
-        interactivity: {
-          detect_on: "window",
-          events: {
-            onhover: {
-              enable: true,
-              mode: "repulse",
-            },
-            onclick: {
-              enable: true,
-              mode: "push",
-            },
-            resize: true,
-          },
-          modes: {
-            grab: {
-              distance: 140,
-              line_linked: {
-                opacity: 1,
-              },
-            },
-            bubble: {
-              distance: 400,
-              size: 40,
-              duration: 2,
-              opacity: 8,
-              speed: 3,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-            push: {
-              particles_nb: 4,
-            },
-            remove: {
-              particles_nb: 2,
-            },
-          },
-        },
-        retina_detect: true,
-      })
     }
 
     // READMORE JS
@@ -244,7 +134,7 @@ window.nagiload = true
 
     // STICKY NAVBAR
     $(window).on("scroll touchmove", function(e) {
-      $(".navbar").toggleClass("stick-me", $(document).scrollTop() > 1)
+      $(".navbar").toggleClass("stick-me", getScrollTop() > 1)
     })
 
     // SWIPER SLIDER
@@ -288,54 +178,24 @@ window.nagiload = true
       var goTo = this.getAttribute("href")
       setTimeout(function() {
         window.location = goTo
-      }, 1000)
+      }, 500)
     })
-
-    // WORKS GRID
-    // $(window).load(function(e) {
-    $(".works-grid").isotope({
-      itemSelector: ".works-grid li",
-      percentPosition: true,
-    })
-    // })
-
-    // WORKS FILTER
-    // $(window).load(function(e) {
-    var $container = $(".works-grid")
-    $container.isotope({
-      filter: "*",
-      animationOptions: {
-        duration: 750,
-        easing: "linear",
-        queue: false,
-      },
-    })
-
-    $(".works-filter a").click(function(e) {
-      $(".works-filter .current").removeClass("current")
-      $(this).addClass("current")
-
-      var selector = $(this).attr("data-filter")
-      $container.isotope({
-        filter: selector,
-        animationOptions: {
-          duration: 750,
-          easing: "linear",
-          queue: false,
-        },
-      })
-      return false
-    })
-    // })
   })
-
   // ODOMETER
-  $(window).scroll(function(e) {
-    $(".odometer").each(function(i) {
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight()
-      var bottom_of_window = $(window).scrollTop() + $(window).height()
-    })
-  })
+  // $(window).scroll(function(e) {
+  //   $(".odometer").each(function(i) {
+  //     var bottom_of_object = $(this).offset().top + $(this).outerHeight()
+  //     var bottom_of_window = $(window).scrollTop() + $(window).height()
+
+  //     /* If the object is completely visible in the window, fade it in */
+  //     if (bottom_of_window > bottom_of_object) {
+  //       $("#1").html("11")
+  //       $("#2").html("870")
+  //       $("#3").html("252")
+  //       $("#4").html("99")
+  //     }
+  //   })
+  // })
 
   // WOW ANIMATION
   wow = new WOW({
---
+++ docs/styles-4d6caf1af9c1f28c8862.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{232:function(n,o,w){},233:function(n,o,w){},247:function(n,o,w){}}]);
+//# sourceMappingURL=styles-4d6caf1af9c1f28c8862.js.map
